df53e252b32ab7672b3cd6a4fd211f3b606bc510
Paco Rampas <pacorampas@gmail.com>
2020-06-01 10:31:01 +0200

    infected new

--- src/game/game.js
+++ src/game/game.js
@@ -101,11 +101,8 @@ export const initGame = function (
     )
 
     this.load.image('ball', process.env.PUBLIC_URL + '/assets/person.png')
-    const urlParams = new URLSearchParams(window.location.search)
-    const infectedAlt = urlParams.get('infectedAlt')
-    
-    const ifectedPath = infectedAlt ? '/assets/infected.png' : '/assets/infected1.png'
-    this.load.image('infected', process.env.PUBLIC_URL + ifectedPath)
+   
+    this.load.image('infected', process.env.PUBLIC_URL + '/assets/infected2.png')
 
     this.load.image('item_mask', process.env.PUBLIC_URL + '/assets/ico-mask.png')
     this.load.image(
